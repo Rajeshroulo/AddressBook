a24a746934d5a27a265f3f6e263f3cd14c1b5acb
Rajeshroulo <rajraval017@gmail.com>
2020-05-28 23:12:27 +0530

    Added feature to search by name

--- src/main/java/addressbook/AddressBook.java
+++ src/main/java/addressbook/AddressBook.java
@@ -11,4 +11,13 @@ public class AddressBook {
         addressBookList.add(person);
     }
 
+    public boolean search(String firstName) {
+        for (int i=0;i<addressBookList.size();i++) {
+            String name = addressBookList.get(i).firstName;
+            if (name.equals(firstName))
+                return true;
+        }
+        return false;
+    }
+
 }
--- src/test/java/addressbook/AddressBookTest.java
+++ src/test/java/addressbook/AddressBookTest.java
@@ -26,4 +26,22 @@ public class AddressBookTest {
         Assert.assertEquals(1,size);
     }
 
+    @Test
+    public void givenPersonsDetailsSearchedByName_whenFound_shouldReturnTrue() {
+        AddressBook addressBook = new AddressBook();
+        addressBook.add("Raj","purohit","Jaipur","Jaipur","Rajasthan",
+                "560008","9363675348");
+        boolean result = addressBook.search("Raj");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPersonsDetailsSearchedName_whenNotFound_shouldReturnFalse() {
+        AddressBook addressBook = new AddressBook();
+        addressBook.add("Raj","Raval","Musheerabad","Hyderabad","Telangana",
+                "456132","9354675354");
+          boolean result = addressBook.search("Rajesh");
+        Assert.assertFalse(result);
+    }
+
 }
